56a827e59960e9a56ee0cec38319aeddf8969621
Denis Babakov <DBabakov@alfabank.ua>
2024-12-18 13:39:48 +0200

    added maven

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ publishing {
             }
             groupId = "com.github.stoplus"
             artifactId = "mysdktest"
-            version = "1.0.1"
+            version = "1.0.2"
         }
     }
     repositories {
