6550edb84767e9b8dc43620444a9c35bd444b1ff
Denis Babakov <DBabakov@alfabank.ua>
2024-12-18 17:26:27 +0200

    added maven

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,11 +22,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "11"
     }
 }
 
@@ -38,7 +38,7 @@ publishing {
             }
             groupId = "com.github.stoplus"
             artifactId = "mysdktest"
-            version = "1.0.9"
+            version = "1.0.10"
         }
     }
     repositories {
